6f9b40e6952d5b97d66701596decb774592e4db2
Lucas Almeida <lucas.contato07@hotmail.com>
2026-09-19 11:38:29 -0300

    Correção lógica porcentagem
    A anterior tinha quebrado a raspadinha

--- raspadinha.js
+++ raspadinha.js
@@ -63,7 +63,7 @@ function getTouchPos(e) {
     const rect = canvas.getBoundingClientRect();
     const scaleX = canvas.width / rect.width;
     const scaleY = canvas.height / rect.height;
-    return { x: (e.touches[0].clientX - rect.left) * scaleX, y: (e.touches[0].top - rect.top) * scaleY };
+    return { x: (e.touches[0].clientX - rect.left) * scaleX, y: (e.touches[0].clientY - rect.top) * scaleY };
 }
 
 // =============================
@@ -274,7 +274,6 @@ if (valorParam.includes('%')) {
     if (valorPremioElement) {
         valorPremioElement.textContent = `(${valorParam} DE DESCONTO)`;
     }
-    // Oculta as regras de valor mínimo quando for porcentagem
     elementosRegras.forEach(el => el.style.display = 'none');
 } 
 // SE FOR VALOR EM REAIS (Ex: valor=500)
@@ -292,7 +291,6 @@ else {
         valorMinimoElement.textContent = `R$ ${valorMinimo.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}`;
     }
 
-    // Garante que a frase de regra continuará visível
     elementosRegras.forEach(el => el.style.display = 'block');
 }
 
